e17564308c27f5319c11399413b57929e39b867b
Akari <akkarinn@qq.com>
2025-03-21 22:22:50 +0800

    Update demo

--- core/src/commonMain/kotlin/com/akari/ulayout/ulpack/Ulpack.kt
+++ core/src/commonMain/kotlin/com/akari/ulayout/ulpack/Ulpack.kt
@@ -2,6 +2,7 @@ package com.akari.ulayout.ulpack
 
 import com.akari.ulayout.dom.DataSource
 import com.akari.ulayout.resource.accessor.*
+import com.akari.ulayout.util.UlayoutJson
 import korlibs.io.file.std.openAsZip
 import korlibs.io.stream.openAsync
 import okio.Path
@@ -10,13 +11,24 @@ import okio.Path.Companion.toPath
 
 class Ulpack private constructor(
     val configure: UlayoutConfigure,
+    val manifest: Manifest?,
     private val delegatedRA: ResourceAccessor = ResourceAccessor,
 ) : ResourceAccessor by (BuiltinResourceAccessor + delegatedRA).catch() {
     companion object {
         fun wrap(
             configure: UlayoutConfigure,
+            manifest: Manifest = Manifest(
+                version = -1,
+                author = "Akari",
+                name = "wrap",
+                description = "The ulpack is wrapped by Ulpack.wrap()",
+            ),
             storageBuilder: MutableMap<Path, String>.() -> Unit = {}
-        ) = Ulpack(configure, buildMap(storageBuilder).asMemoryRA())
+        ) = Ulpack(
+            configure = configure,
+            manifest = manifest,
+            delegatedRA = buildMap(storageBuilder).asMemoryRA()
+        )
 
         suspend fun fromZip(
             domSource: DataSource
@@ -25,9 +37,15 @@ class Ulpack private constructor(
             val zra = ZipResourceAccessor(zip)
             val configureJson = zra.readText("layout.json".toPath())
             val configure = UlayoutConfigure.parse(configureJson)
+            val manifestJson = zra.readTextOrNull("manifest.json".toPath())
+            val manifest = manifestJson?.let {
+                UlayoutJson.decodeFromString<Manifest>(manifestJson)
+            }
+
             return Ulpack(
-                configure,
-                zra
+                configure = configure,
+                manifest = manifest,
+                delegatedRA = zra
             )
         }
     }
